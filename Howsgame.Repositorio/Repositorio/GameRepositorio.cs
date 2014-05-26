3d701c4c03a0bb2b45164f2fe53f15899a8b0283
Rodrigo <unknown-user@fe80::e450:feb2:b6e4:7778%10>
2014-05-26 19:45:39 -0300

    oi

--- Howsgame.Repositorio/Repositorio/GameRepositorio.cs
+++ Howsgame.Repositorio/Repositorio/GameRepositorio.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using Howsgame.Dominio;
 
 namespace Howsgame.Repositorio
 {
-	public class GameRepositorio
+	public class GameRepositorio:RepositorioBase
 	{
 		public GameRepositorio ()
 		{
 
 		}
+		/*public void Adicionar(Game game)
+			{
+
+				Executar<int> ("Insert Game(Nome,Release,categoriaID,plataformaID,avaliadorID) values(@Nome,@Release,@categoriaID,@plataformaID,@avaliadorID)", 
+					new{game.Nome,game.Release,game.Categoria.Id,game.Plataforma.Id,game.Avaliador.Id});
+
+			}
+			public void Atualizar(Game game)
+			{
+				Executar<int> ("Update Game set Nome = @Nome,Release = @release ,categoriaID= @categoriaID,plataformaID = @plataformaID,avaliadorID = @avaliadorID",
+					new{game.Nome,game.Release,game.Categoria.Id,game.Plataforma.Id,game.Avaliador.Id});
+			}*/
 	}
 }
 
