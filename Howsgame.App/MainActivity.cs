51f7ba0df4f30f9bdba4fb64b54f983b8b40f662
Josue <rodrigohs29@gmail.com>
2014-05-26 19:37:51 -0300

    Menu Lateral
    Menu Lateral

--- Howsgame.App/MainActivity.cs
+++ Howsgame.App/MainActivity.cs
@@ -11,21 +11,14 @@ namespace Howsgame.App
 	[Activity (Label = "Howsgame.App", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
-			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+			var menu = FindViewById<FlyOutContainer> (Resource.Id.FlyOutContainer);
+			var menuButton = FindViewById (Resource.Id.MenuButton);
+			menuButton.Click += (sender, e) => {
+				menu.AnimatedOpened = !menu.AnimatedOpened;
 			};
 		}
 	}
